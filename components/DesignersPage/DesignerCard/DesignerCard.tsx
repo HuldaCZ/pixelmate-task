a1f04c625d7d7ef9081ff14d28ab16943afc820e
HuldaCZ <phulinsky@gmail.com>
2021-11-17 22:47:41 +0100

    Designer Card - without props

--- components/DesignersPage/DesignerCard/DesignerCard.tsx
+++ components/DesignersPage/DesignerCard/DesignerCard.tsx
@@ -1,28 +1,37 @@
 import React from "react";
 import Button from "../../Button/Button";
 
-const DesignerCard = (props:any) => {
-    return (
-        <div className="card">
-        <div className="card-image">
+import IconLocation from "../../../public/img/icon-location.svg";
+
+import styles from "./DesignerCard.module.scss";
+
+const DesignerCard = (props: any) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.card__content}>
+        <div className={styles.card__content__header}>
+          <p className={styles.card__content__header__userId}>r.barrett99</p>
+          <p className={styles.card__content__header__name}>Richard Barrett</p>
+          <p className={styles.card__content__header__email}>r.barrett99@email.com</p>
         </div>
-        <div className="card-content">
-            <div className="media">
-            <div className="media-content">
-                <p className="title is-4">Richard Barrett</p>
-                <p className="subtitle is-6">r.barrett99@email.com</p>
-            </div>
-            </div>
-            <div className="content">
-            {props.description}
-            <br />
-            <a href={props.link}>
-                <Button text="View Profile" onClick={() => {}} design="outline-primary" />
-            </a>
-            </div>
+        <div className={styles.card__content__adress}>
+          <div className={styles.card__content__adress__col_i}>
+            <IconLocation />
+          </div>
+          <div className={styles.card__content__adress__col}>
+            <p className={styles.card__content__adress__city}>Praha</p>
+            <p className={styles.card__content__adress__street}>Na Lysinkách 443/57</p>
+            <p className={styles.card__content__adress__street}>147 00 Praha - Hodovičky</p>
+          </div>
         </div>
+        <div className={styles.card__content__web}>www.petrhuli.cz</div>
+
+        <div className={styles.card__content__button}>
+          <Button text="View Profile" onClick={() => {}} design="outline-primary" />
         </div>
-    );
-    }
+      </div>
+    </div>
+  );
+};
 
 export default DesignerCard;
